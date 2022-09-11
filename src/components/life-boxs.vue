37120a07e8498f35adb5732ca6f90c874adb679a
programmer-heeney <programmer.heeney@gmail.com>
2022-09-11 23:57:23 +0900

    Refact(boxs): 나머지 박스 색상 회색으로 변경 및 배경색 판별 함수 추가
    - 흰색이 눈이 아프다는 의견을 받아서 회색으로 변경
    - tailwindcss 배경색 우선순위를 고려

--- src/components/life-boxs.vue
+++ src/components/life-boxs.vue
@@ -2,17 +2,20 @@
 interface Props {
     weeks: any;
 }
-const YEARS = 81;
+const props = defineProps<Props>();
+
+const YEARS = 91;
 const WEEKS_A_YEAR = 52;
 
-const props = defineProps<Props>();
+const isBeforeCurrent = (year:number, week:number) => ((((year-1)*WEEKS_A_YEAR) + week) <= props.weeks)
+
 </script>
 <template>
     <ul class="flex flex-col mt-10">
         <li v-for="year in YEARS" :key="year" class="flex items-center">
             {{year-1 < 10 ? `0${year-1}`:year-1}}
-            <div v-for="week in WEEKS_A_YEAR" :key="week" class="w-[5px] h-[5px] bg-green-50 m-1 group"
-            :class="{'bg-green-900': (((year-1)*WEEKS_A_YEAR) + week) <= weeks, 'bg-yellow-300': week%5 === 0}"
+            <div v-for="week in WEEKS_A_YEAR" :key="week" class="w-[5px] h-[5px] m-1 group"
+            :class="{'bg-green-900': isBeforeCurrent(year, week), 'bg-yellow-300': week%5 === 0, 'bg-stone-500': !isBeforeCurrent(year, week)}"
             >
                 <div class="relative bottom-[30px] flex flex-col items-center hidden mb-6 group-hover:flex">
                     <span class="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg">{{(((year-1)*WEEKS_A_YEAR) + week)}}</span>
