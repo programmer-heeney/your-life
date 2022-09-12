a69ea60dbbf8516775b515d48932bc0d6549dc80
programmer-heeney <programmer.heeney@gmail.com>
2022-09-12 14:28:00 +0900

    Style(boxs): 모바일에서는 나이가 보이지 않게 설정
    - 모바일에서 스크롤이 길어지지 않도록 스크롤 변경
    - 툴팁에 만나이와 주수 표시
    - 라이트/다크 테마 구분 생략

--- src/components/life-boxs.vue
+++ src/components/life-boxs.vue
@@ -11,17 +11,22 @@ const isBeforeCurrent = (year:number, week:number) => ((((year-1)*WEEKS_A_YEAR) 
 
 </script>
 <template>
-    <ul class="flex flex-col mt-10">
-        <li v-for="year in YEARS" :key="year" class="flex items-center">
-            {{year-1 < 10 ? `0${year-1}`:year-1}}
-            <div v-for="week in WEEKS_A_YEAR" :key="week" class="w-[5px] h-[5px] m-1 group"
+    <ul class="flex flex-col mt-10 w-80 md:w-full">
+        <span class="text-left md:hidden">만 0세 부터</span>
+        <li v-for="year in YEARS" :key="year" class="flex items-center justify-between">
+            <span class="hidden md:block">{{year-1 < 10 ? `0${year-1}`:year-1}}</span>
+            <div v-for="week in WEEKS_A_YEAR" :key="week" class="w-[5px] h-[5px] mb-[2px] md:mb-0 group"
             :class="{'bg-green-900': isBeforeCurrent(year, week), 'bg-yellow-300': week%5 === 0, 'bg-stone-500': !isBeforeCurrent(year, week)}"
             >
-                <div class="relative bottom-[30px] flex flex-col items-center hidden mb-6 group-hover:flex">
-                    <span class="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg">{{(((year-1)*WEEKS_A_YEAR) + week)}}</span>
+                <div class="relative bottom-[45px] flex flex-col items-center hidden mb-6 group-hover:flex">
+                    <span class="relative w-20 z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg">
+                        {{`만 ${year-1}세:`}}<br/>
+                        {{`${week}주차`}}
+                    </span>
                     <div class="w-3 h-3 -mt-2 rotate-45 bg-black"></div>
                 </div>  
             </div>
         </li>
+        <span class="text-right md:hidden">만 91세 까지</span>
     </ul>
 </template>
